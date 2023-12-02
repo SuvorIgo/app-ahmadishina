23e7e6cce716eafc0cdd3777fef81f59f8b8809e
Igor Suvorov <suekandig@mail.ru>
2023-12-02 20:30:45 +0300

    Инициализация сервиса, контроллера и модуля warehouses

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { WarehousesModule } from './warehouses/warehouses.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    WarehousesModule,
   ],
   controllers: [AppController],
   providers: [AppService],

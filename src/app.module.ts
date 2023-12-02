3478d00f22d552fba838a1443103d26c2b741780
Igor Suvorov <suekandig@mail.ru>
2023-12-02 20:42:13 +0300

    Добавление connect into database

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { WarehousesModule } from './warehouses/warehouses.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    MongooseModule.forRoot(
+      `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+    ),
     WarehousesModule,
   ],
   controllers: [AppController],
